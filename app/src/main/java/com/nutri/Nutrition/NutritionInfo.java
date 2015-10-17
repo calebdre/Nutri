f913fe1b9c0805aa784a529f2c48f974616b7b13
Graeme Lowe <Graeme@wireless-guest-123.nat.emory.edu>
2015-10-17 02:05:04 -0400

    added nutrition info

---
+++ app/src/main/java/com/nutri/Nutrition/NutritionInfo.java
@@ -0,0 +1,88 @@
+
+public class NutritionInfo {
+	public String calories, protein, fat, carbs, sugars, calcium, potassium, sodium, vitA, vitC;
+	public String[] allFields = new String[]{
+		"calories", "protein", "fat", "carbs", "sugars", "calcium", "potassium", "sodium", "vitA", "vitC"
+	};
+	
+	public String getCalories() {
+		return calories;
+	}
+
+	public void setCalories(String calories) {
+		this.calories = calories;
+	}
+
+	public String getProtein() {
+		return protein;
+	}
+
+	public void setProtein(String protein) {
+		this.protein = protein;
+	}
+
+	public String getFat() {
+		return fat;
+	}
+
+	public void setFat(String fat) {
+		this.fat = fat;
+	}
+
+	public String getCarbs() {
+		return carbs;
+	}
+
+	public void setCarbs(String carbs) {
+		this.carbs = carbs;
+	}
+
+	public String getSugars() {
+		return sugars;
+	}
+
+	public void setSugars(String sugars) {
+		this.sugars = sugars;
+	}
+
+	public String getCalcium() {
+		return calcium;
+	}
+
+	public void setCalcium(String calcium) {
+		this.calcium = calcium;
+	}
+
+	public String getPotassium() {
+		return potassium;
+	}
+
+	public void setPotassium(String potassium) {
+		this.potassium = potassium;
+	}
+
+	public String getSodium() {
+		return sodium;
+	}
+
+	public void setSodium(String sodium) {
+		this.sodium = sodium;
+	}
+
+	public String getVitA() {
+		return vitA;
+	}
+
+	public void setVitA(String vitA) {
+		this.vitA = vitA;
+	}
+
+	public String getVitC() {
+		return vitC;
+	}
+
+	public void setVitC(String vitC) {
+		this.vitC = vitC;
+	}
+	
+}
